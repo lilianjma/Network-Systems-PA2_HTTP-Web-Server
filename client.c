ca5dc43d4e5e129f620a2b51ef3cd9f6b54ece41
Lilian Ma <lilian.j.ma@gmail.com>
2025-02-19 22:46:38 -0700

    server is sending multiple of the same file content after GET is correctly called, i'm stuck :(

--- client.c
+++ client.c
@@ -47,7 +47,9 @@ int main(int argc, char **argv)
 
 	while (fgets(sendline, MAXLINE, stdin) != NULL)
 	{
-
+		if (strcmp(sendline, "exit\n") == 0) {
+			exit(0);
+		}
 		send(sockfd, sendline, strlen(sendline), 0);
 
 		if (recv(sockfd, recvline, MAXLINE, 0) == 0)
@@ -58,6 +60,8 @@ int main(int argc, char **argv)
 		}
 		printf("%s", "String received from the server: ");
 		fputs(recvline, stdout);
+		bzero(sendline, sizeof(sendline));
+		printf("Line zeroed\n");
 	}
 
 	exit(0);
--- server.c
+++ server.c
@@ -5,10 +5,29 @@
 #include <netinet/in.h>
 #include <string.h>
 #include <unistd.h>
+#include <fcntl.h>
+#include  <signal.h>
 
-#define MAXLINE 4096 /*max text line length*/
-#define LISTENQ 8	 /*maximum number of client connections*/
+#define MAXLINE 			4096 /*max text line length*/
+#define LISTENQ 			8	 /*maximum number of client connections*/
+#define BUF_SIZE			64*1024
+#define ROOT				"./www/"
+#define GET_METHOD	 		"GET"
+#define VERSION_0 			"HTTP/1.0"
+#define VERSION_1 			"HTTP/1.1"
+#define CRLF 				"\r\n"
 
+int serverfd = -1;
+
+void INThandler(int);
+void command_handler(int socketfd, char buf[]);
+
+/**
+ * Method: 	main
+ * Uses: 	Recieve connection requests
+ * 			Fork for every connection
+ * 			Parse connections
+ */
 int main(int argc, char **argv)
 {
 	int listenfd, connfd, n;
@@ -25,6 +44,8 @@ int main(int argc, char **argv)
 	}
 	portno = atoi(argv[1]);
 
+	signal(SIGINT, INThandler);
+
 	// Create a socket for the soclet
 	// If sockfd<0 there was an error in the creation of the socket
 	if ((listenfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
@@ -32,6 +53,7 @@ int main(int argc, char **argv)
 		perror("Problem in creating the socket");
 		exit(2);
 	}
+	serverfd = listenfd;
 
 	// preparation of the socket address
 	servaddr.sin_family = AF_INET;
@@ -48,7 +70,6 @@ int main(int argc, char **argv)
 
 	while (1)
 	{
-
 		clilen = sizeof(cliaddr);
 		// accept a connection
 		connfd = accept(listenfd, (struct sockaddr *)&cliaddr, &clilen);
@@ -65,16 +86,126 @@ int main(int argc, char **argv)
 
 			while ((n = recv(connfd, buf, MAXLINE, 0)) > 0)
 			{
-				printf("%s", "String received from and resent to the client:");
+				printf("String received from the client: ");
 				puts(buf);
-				send(connfd, buf, n, 0);
+				command_handler(connfd, buf);
+				printf("exited commandhandler\n");
 			}
 
+			printf("in here 1\n");
 			if (n < 0)
 				printf("%s\n", "Read error");
+			
 			exit(0);
 		}
 		// close socket of the server
+		printf("Socket closed\n");
 		close(connfd);
 	}
+}
+
+void  INThandler(int sig)
+{
+    signal(sig, SIG_IGN);
+	printf("\nServer exiting...\n");
+	if (serverfd != -1) {
+        close(serverfd);
+        printf("Server socket closed.\n");
+    }
+	 exit(0);
+}
+
+int wordcount(char* bufptr) {
+	/* count the number of words in the command line */
+	char *ptrcpy = bufptr;
+	int count = 0;
+	int inWord = 0;
+
+	while (*ptrcpy != '\0')
+	{
+		if (*ptrcpy == ' ')
+		{
+			inWord = 0;
+		}
+		else if (!inWord)
+		{
+			inWord = 1;
+			count++;
+		}
+		ptrcpy++;
+	}
+
+	return count;
+}
+
+/********************** REQUEST HEADER FORMAT **********************
+ * HTTP/1.1 200 OK \r\n
+ * Content-Type: <> \r\n # Tells about the type of content and the formatting of <file contents> 
+ * Content-Length:<> \r\n # Numeric value of the number of bytes of <file contents>
+ * \r\n<file contents>
+*/
+
+/**
+ * Method:	command_handler
+ * Uses:	
+ */
+void command_handler(int connfd, char* buf) {
+	
+	char method[12]; char uri[256]; char version[12]; // User input buffers
+	char filepath[512];
+	printf("In commandhandler\n");
+
+	// Error if incorrect number of args
+	int argc = wordcount(buf);
+	if (argc != 3) {
+		char* response = "400 Bad Request\r\n\r\n";
+		send(connfd, response, strlen(response), 0);
+		return;
+	}
+
+    // Parse request from client
+    sscanf(buf, "%s %s %s", method, uri, version);
+
+	// Error if not using GET
+	if (strcmp(method, GET_METHOD)) {
+		char* response = "405 Method Not Allowed\r\n\r\n";
+		send(connfd, response, strlen(response), 0);
+		return;
+	}
+
+	// Error if wrong versions
+	if ((strcmp(version, VERSION_0) != 0) && (strcmp(version, VERSION_1) != 0)) {
+		char* response = "505 HTTP Version Not Supported\r\n\r\n";
+		send(connfd, response, strlen(response), 0);
+		return;
+	}
+
+	// Create file path
+	strcpy(filepath, ROOT);
+    strcat(filepath, uri);
+
+    // Open file and send response
+    int fd = open(filepath, O_RDONLY);
+	printf("%s\n", filepath);
+
+    if (fd == -1) { // File not found
+        char* response = "HTTP/1.1 404 Not Found\r\n\r\n"; // TODO do i need to add "\r\nContent-Length: 0"?
+		send(connfd, response, strlen(response), 0);
+    }
+	else { 			// File found
+        char* header = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n";
+        send(connfd, header, strlen(header), 0);
+
+        // Send file contents
+        char file_buffer[BUF_SIZE];
+        ssize_t bytes;
+		int offset = 0;
+		
+        bytes = pread(fd, file_buffer, BUF_SIZE, offset);
+		send(connfd, file_buffer, bytes, 0);
+		offset += BUF_SIZE;
+		bzero(file_buffer, sizeof(file_buffer));
+    }
+	close(fd);
+	printf("File, %s, closed\n", filepath);
 }
